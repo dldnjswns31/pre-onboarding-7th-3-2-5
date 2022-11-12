7c4d86d1390f1730f7172d3aa5b6863cf9344228
baechanghyeon <pek5302@naver.com>
2022-11-12 14:33:58 +0900

    feat: 초기세팅

---
+++ src/pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <title>투자관리 서비스</title>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
