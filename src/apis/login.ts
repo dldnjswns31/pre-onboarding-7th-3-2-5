47150abb26c4f903dfe0b59ebc564d6615e1c55d
jaeyeong815 <jae0ee@naver.com>
2022-11-17 00:04:53 +0900

    feat: 계좌, 사용자 정보 가져오는 api 작성 (추후 분리 예정)

--- src/apis/login.ts
+++ src/apis/login.ts
@@ -1,4 +1,4 @@
-import { setSessionStorage } from '@/utils/token';
+import { getSessionStorage, setSessionStorage } from '@/utils/token';
 import axios, { AxiosRequestConfig } from 'axios';
 
 const instance = axios.create({ baseURL: '/api' });
@@ -8,6 +8,41 @@ export const login = async (email: string, password: string) => {
     const { data } = await instance.post('/login', { email, password });
     setSessionStorage('token', data.accessToken);
     return data.user;
+  } catch (err) {
+    console.log('error : ', err);
+  }
+};
+
+// TODO 추후 분리 예정
+export const getAccountList = async () => {
+  const token = getSessionStorage('token');
+
+  try {
+    const { data } = await instance.get('/accounts', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      params: {
+        page: 7,
+      },
+    });
+    return data;
+  } catch (err) {
+    console.log('error : ', err);
+  }
+};
+
+// TODO 추후 분리 예정
+export const getUserList = async () => {
+  const token = getSessionStorage('token');
+
+  try {
+    const { data } = await instance.get('/users', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    console.log('data', data);
   } catch (err) {
     console.log('error : ', err);
   }
