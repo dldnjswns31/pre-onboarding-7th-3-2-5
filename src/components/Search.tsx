8296696223c68a8b5f05d99c06690ab62d54e528
jaeyeong815 <jae0ee@naver.com>
2022-11-17 18:34:12 +0900

    feat: 검색 기능 구현

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,24 +1,36 @@
 import { getSearchData } from '@/apis/login';
+import { accountState } from '@/recoil/accountState';
 import { useState } from 'react';
+import { useSetRecoilState } from 'recoil';
 
 export default function Search() {
+  const setAccountList = useSetRecoilState(accountState);
   const [searchKeyword, setKeyword] = useState<string>('');
+  const [isShow, setIsShow] = useState<boolean>(false);
+
   const onChangeHandle = (e) => {
     setKeyword(e.target.value);
   };
 
   const onSubmitHandle = (e) => {
     e.preventDefault();
-    getSearchData(searchKeyword).then((res) => console.log(res));
+    getSearchData(searchKeyword).then((res) => setAccountList(res));
+    setIsShow(true);
     setKeyword('');
   };
 
-  console.log('searchKeyword', searchKeyword);
+  const onClickHandle = (e) => {
+    e.preventDefault();
+    setKeyword('');
+    getSearchData(searchKeyword).then((res) => setAccountList(res));
+    setIsShow(false);
+  };
 
   return (
     <form onSubmit={onSubmitHandle}>
-      <input type="text" value={searchKeyword} onChange={onChangeHandle} />
+      <input type="text" value={searchKeyword} onChange={onChangeHandle} placeholder="계좌명을 검색해보세요." />
       <input type="submit" value="검색" />
+      {isShow && <input type="button" value="전체 계좌보기" onClick={onClickHandle} />}
     </form>
   );
 }
