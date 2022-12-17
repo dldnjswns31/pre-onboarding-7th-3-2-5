d4b495586aaad034b584a1b0b737df11e179e20b
dldnjswns31 <dldnjswns32@gmail.com>
2022-12-17 20:24:29 +0900

    feat: next.config에서 api 주소 변경

--- next.config.js
+++ next.config.js
@@ -6,8 +6,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        // destination: 'https://december-server.fly.dev/:path*',
-        destination: 'http://localhost:4000/:path*',
+        destination: 'https://december-server.fly.dev/:path*',
       },
     ];
   },
